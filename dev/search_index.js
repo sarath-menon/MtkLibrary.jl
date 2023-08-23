2eaf3ddfefc2591d12989ae7ecebdfe7692aecc3
Documenter.jl <documenter@juliadocs.github.io>
2023-08-23 08:20:49 +0000

    build based on 94fd1d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MtkLibrary","category":"page"},{"location":"#MtkLibrary","page":"Home","title":"MtkLibrary","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MtkLibrary.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MtkLibrary]","category":"page"}]
+}
