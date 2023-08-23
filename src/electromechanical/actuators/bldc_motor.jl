9f830eb5b29270281845f81884ef1dcc4fa694c7
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-08-23 21:22:48 +0200

    removed param from func arg

--- src/electromechanical/actuators/bldc_motor.jl
+++ src/electromechanical/actuators/bldc_motor.jl
@@ -6,7 +6,7 @@ function BldcMotorPropellerPair(; name)
     # translation
     @variables t thrust_cmd(t) = 0 thrust_output(t) = 0
 
-    @named first_order_system = FirstOrder(k=1.0, T=τ)
+    @named first_order_system = FirstOrder(k=1.0)
 
     eqns = [thrust_cmd ~ first_order_system.u,
         thrust_output ~ first_order_system.y]
